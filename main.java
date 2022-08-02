edb91372da86d4c63a4ef4513d510c19db8f92e1
Afrem Yarbug <95577685+AfremYarbug@users.noreply.github.com>
2022-08-02 19:24:16 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+package main;
+public class Main {
+
+    public static void main(String[] args) {
+        
+      // for (<<TYP>> <<VARIABLENNAME>> : <<AUSDRUCK>>)
+      
+      //Traditionelle Schleifen-Notation
+      int summe = 0;
+      for (int i = 0; i < werte.length; i++) {
+        summe = summe + werte[i];
+      }
+      
+      // Neue, vereinfachte Schleifen-Notation
+      int summe = 0;
+      for (int x : werte) {
+        summe = summe + werte[i];
+      }
+      
+      /*ist sorgfältig mit Indexgrenzen und Komponentenzugriffen, da man Laufzeitfehler bei der Überschreitung der Indexbereiche macht...*/
+      
+    }
+    
+}
